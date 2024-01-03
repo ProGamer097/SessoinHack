9f9154b53715f88241f870c2401933589e2f9f53
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2024-01-03 12:08:35 +0530

    Update bcast.py

--- AMBOTSSHACK/modules/bcast.py
+++ AMBOTSSHACK/modules/bcast.py
@@ -15,11 +15,7 @@ from pyrogram.types import (
 )
 import random
 from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid
-    API_ID = "20136640"
-    API_HASH = "32fa482ffd34ceefec26b7ccd73c4fbb"
-    TOKEN = "6372858522:AAHxQ8VKp6FC1X_U6GEbERCqRQjOxRJrB8w"
     MONGO_URL = "mongodb+srv://AMBOT:AMBOT@ambot.uecutzy.mongodb.net/?retryWrites=true&w=majority"
-    START_PIC = "https://graph.org/file/5312b0262def592d4505e.jpg"
     SUDOERS = filters.user(["5360305806"])
     OWNER_ID = "5360305806"
 mongo = MongoCli(MONGO_URL)
