a6b1062cd701087ec9bdbfa804f8dd075e4c553d
AMBOTYTOP <134328176+AMBOTYTOP@users.noreply.github.com>
2024-04-08 22:09:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ class Config:
     TOKEN = "6372858522:AAGOvHBU4V63ap346wfSNJRNaXJKwDuxqhI"
     MONGO_URL = "mongodb+srv://AMBOT:AMBOT@ambot.uecutzy.mongodb.net/?retryWrites=true&w=majority"
     START_PIC = "https://graph.org/file/5312b0262def592d4505e.jpg"
-    SUDOERS = filters.user(["5360305806"])
-    OWNER_ID = "5360305806"
+    SUDOERS = filters.user(["5916859256"])
+    OWNER_ID = "5916859256"
